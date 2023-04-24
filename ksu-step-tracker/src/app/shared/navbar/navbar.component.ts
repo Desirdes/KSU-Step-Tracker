102fba65c74e286d87eb3417622e92247cec0fed
David Sanford <dsanfor5@students.kennesaw.edu>
2023-04-24 12:22:13 -0400

    Merge fixes

--- ksu-step-tracker/src/app/shared/navbar/navbar.component.ts
+++ ksu-step-tracker/src/app/shared/navbar/navbar.component.ts
@@ -3,7 +3,6 @@ import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { filter, pairwise } from 'rxjs/operators'
 import { AppComponent } from 'src/app/app.component';
 import { APIService } from '../APIService';
-import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
@@ -11,32 +10,12 @@ import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  //public sidenav;
-  @Output() public sideNavToggle = new EventEmitter();
-  //@ViewChild('sidenav', {static:false}) sidenav: MatSidenav;
 
   constructor(
     public appComponent: AppComponent,
     private apiService: APIService,
     private router: Router
   ) { }
-
-  //Open main nav menu
-  collapsed = false; /* false by default, since hidden */
-
-  //Open sidenav menu
-  sideCollapsed: boolean = false;
-
-  //Toggle Nav Menu
-  toggleMenu() {
-      this.collapsed = !this.collapsed;
-   }
-
-   //Toggle Sidenav Menu
-   public toggleSideMenu = () => {
-    this.sideNavToggle.emit();
-    //this.sideCollapsed  =!this.sideCollapsed;
- }
 
   ngOnInit() {}
 
